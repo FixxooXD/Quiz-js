dc52e73e64bfbe56290f836bec82be48eb4ae204
Fixxoo <rajpalmal40@gmail.com>
2022-11-24 13:04:15 +0530

    Fixed the New Feature(Counter)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const Btn = document.getElementById("btn");
+const NextQueBtn = document.getElementById("btn");
 // selected all the options as a one
 const optionBtn = document.querySelectorAll("#option");
 const question = document.querySelector("#question");
@@ -11,50 +11,73 @@ const op1 = document.getElementById("op1");
 const QuesCount = document.getElementById("questionCount");
 const startBtn = document.getElementById('startBtn');
 const rulesPopup = document.querySelector('#rules');
-const timer = document.getElementById('timeleft');
+const timerLeft = document.getElementById('timeleft');
 let correctAns;
 let firstOption;
 let secondOption;
 let thirdOption;
 let fourtOption;
-let questionsCounter = -1;
+let questionsCounter = 0;
+let counter;
 
-// function startTimer(time){
-//   counter = setInterval(timer, 1000);
-//   function timer(){
-//     timer.textContent = time--;
-//     if(time < 9){ //if timer is less than 9
-//       let addZero = timeCount.textContent;
-//       timer.innerHTML = "0" + addZero; //add a 0 before time value
-//      }
-//      if(time < 0){ //if timer is less than 0
-//       clearInterval(counter); //clear c
-//   }
-// }
-// }
+// This Flag will tell that the user didn't select any option and he clicked the nextQuestion button if it is false
+let optionsNotClickedFlag ;
 
-  var seconds = 15;
-var refreshIntervalId = setInterval(stopwatch, 1000);
-function stopwatch() {
-  timer.innerHTML = seconds--;
-  if (seconds < 0) {
-    doSomething();
-    clearInterval(refreshIntervalId)
+function startTimer(time){
+  optionsNotClickedFlag = false;
+  counter = setInterval(timer, 1000);
+  function timer(){
+    timerLeft.innerHTML = time;
+    console.log(time)
+    if(time <= 9){ //if timer is less than 9
+      timerLeft.innerHTML = "0" + time; //add a 0 before time value
+     }
+     time--;
+     if(time < 0){ //if timer is less than 0
+      clearInterval(counter); //TO Stop Counter
+      doSomething();
   }
 }
-
+}
 
 startBtn.addEventListener('click', ()=>{
   rulesPopup.style.display= 'none'
   getquiz()
+  optionsNotClickedFlag = false;
 })
 
+//  Its an onclick function added the the  HtmlFile it will disable all the option Btn if any of the button is clicked
+const doSomething = () =>{
+  clearInterval(counter);
+  optionsNotClickedFlag = true;
+  option1.disabled = true;
+  option2.disabled = true;
+  option3.disabled = true;
+  option4.disabled = true;
 
+  //  this feature is also added symontaniously onclick of any of the options
+  // it will turn of bordercolor of the correct option wheather it was  clicked or not so the if the user click the wrong option
+  // it show both  the worong as will as the correct option
+  if (option1.innerHTML == correctAns) {
+    option1.style.borderColor = "green";
+    //   option1.innerHTML = `${firstOption}  <i id="check" class="fa-regular fa-circle-check p-1"></i>`;
+  }
+  if (option2.innerHTML == correctAns) {
+    option2.style.borderColor = "green";
+    // option2.innerHTML += `${secondOption}  <i id="check" class="fa-regular fa-circle-check p-1"></i>`;
+  }
+  if (option3.innerHTML == correctAns) {
+    option3.style.borderColor = "green";
+    //       option3.innerHTML = `${thirdOption}  <i id="check" class="fa-regular fa-circle-check p-1"></i>`;
+  }
+  if (option4.innerHTML == correctAns) {
+    option4.style.borderColor = "green";
+    //         option4.innerHTML = `${fourtOption}  <i id="check" class="fa-regular fa-circle-check p-1"></i>`;
+  }
+}
 
-// whenever the file get loaded it will automatically run the getquiz()
-// document.addEventListener("DOMContentLoaded", getquiz);
-
-Btn.addEventListener("click", getquiz);
+// Calling getquiz() Onlcik ON the NextQue Btn;
+NextQueBtn.addEventListener("click", getquiz);
 
 async function getquiz() {
   const fetchData = await fetch(
@@ -128,12 +151,19 @@ async function getquiz() {
   questionsCounter += 1;
   QuesCount.innerHTML = questionsCounter;
 
+  // doSomething()
+  // A Option is clicked
+  if(optionsNotClickedFlag == true)
+  startTimer(15); //calling startTimer function
+ else{
+  // UseR Didn't select anyof the option
+  // this will stop the counter at the moment when the nextQuestion  is clicked
+  clearInterval(counter);
+  // At the same Point it will Restart the counter
   startTimer(15);
+ }
 
-  // refreshIntervalId = setInterval(stopwatch, 1000);
 
-
-  // refreshIntervalId = setInterval(stopwatch, 1000);
 }
 // most Important Part Because i had created "optionBtn" as a QuerySelectorAll I have to Iterate individulally
 // to each optionsBtns so i had created a forEach  loop to itereation over each options
@@ -153,34 +183,3 @@ optionBtn.forEach((options) => {
     }
   });
 });
-
-//  Its an onclick function added the the  HtmlFile it will disable all the option Btn if any of the button is clicked
-const doSomething = () =>{
-  clearInterval(refreshIntervalId)
-  // (A) DISABLE THE BUTTON
-  option1.disabled = true;
-  option2.disabled = true;
-  option3.disabled = true;
-  option4.disabled = true;
-
-  //  this feature is also added symontaniously onclick of any of the options
-  // it will turn of bordercolor of the correct option wheather it was  clicked or not so the if the user click the wrong option
-  // it show both  the worong as will as the correct option
-  if (option1.innerHTML == correctAns) {
-    option1.style.borderColor = "green";
-    //   option1.innerHTML = `${firstOption}  <i id="check" class="fa-regular fa-circle-check p-1"></i>`;
-  }
-  if (option2.innerHTML == correctAns) {
-    option2.style.borderColor = "green";
-    // option2.innerHTML += `${secondOption}  <i id="check" class="fa-regular fa-circle-check p-1"></i>`;
-  }
-  if (option3.innerHTML == correctAns) {
-    option3.style.borderColor = "green";
-    //       option3.innerHTML = `${thirdOption}  <i id="check" class="fa-regular fa-circle-check p-1"></i>`;
-  }
-  if (option4.innerHTML == correctAns) {
-    option4.style.borderColor = "green";
-    //         option4.innerHTML = `${fourtOption}  <i id="check" class="fa-regular fa-circle-check p-1"></i>`;
-  }
-}
-
